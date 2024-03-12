e4406827c23b57b1ad0790a79cc2f0f54fc508fe
caiogomesdevv <162522614+caiogomesdevv@users.noreply.github.com>
2024-03-12 16:45:32 -0300

    Opção de saida
    incluso no sistema a opção de saída para o usuário

--- cartorio.c
+++ cartorio.c
@@ -109,6 +109,7 @@ int main()
 	   printf("\t1 - Registrar nomes\n");
 	   printf("\t2 - Consultar nomes\n");
 	   printf("\t3 - Deletar nomes\n\n");
+	   prinft("\t4 - Sair do sistema\n\n");
 	   printf("\t software de caio :P\n\n");
        printf("Opção: ");// fim do menu
     
@@ -129,6 +130,11 @@ int main()
 		
 		 case 3:
 		 deletar();
+		 break;
+		 
+		 case 4:
+		 printf("Obrigado por ultilizar o sistema!\n");
+		 return 0;
 		 break;
 	  	     
 		 default:
